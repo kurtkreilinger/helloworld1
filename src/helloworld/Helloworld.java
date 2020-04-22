5668e469a45c797d7182a7e9350ec939e59bbbb9
Kurt Kreilinger <k.kreilinger@gmail.com>
2020-04-22 16:39:12 +0200

    Great commit

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -6,6 +6,7 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello universe!");
+        System.out.println("Hi folks!");
     }
     
 }
